3ee45965f74dd81e5c973f0ee3b9398d2dcaa0c4
Stella <morenikejiakinyemi6@gmail.com>
2026-03-31 17:11:46 +0100

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,46 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach((item) => {
+      const numericCost = parseFloat(item.cost.substring(1))
+      const subTotal = numericCost * item.quantity
+      total += subTotal;
+    }
+  )
+  return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e)
   };
 
-
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
-  };
+   if(item.quantity > 1) {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}))
+   } else {
+    dispatch(removeItem(item.name));
+   }
+
+   };
+  
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const numericCost = parseFloat(item.cost.substring(1))
+    return numericCost * item.quantity;
   };
 
   return (
@@ -62,6 +81,7 @@ const CartItem = ({ onContinueShopping }) => {
     </div>
   );
 };
+
 
 export default CartItem;
 
